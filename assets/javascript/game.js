816267ede54a5324e2fc57e365e804694b8c9ecd
Will H <wandrewhouston@gmail.com>
2018-09-09 17:23:01 -0400

    added event listener and letter check

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,41 @@
 window.onload = function(){
-//storage
-var numWins = 0
-var numGuesses = 10
-var lettersGuessed = []
-var wordList = ["dragonborn", "dwarf", "elf", "gnome", "halfelf", "halforc", "halfling", "human", "tiefling", "barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "warlock", "wizard"]
-var currentWord = ""
-var correct = []
-//display
-var disWins = document.getElementById("#dis-wins")
-var disNumGuess = document.getElementById("#num-guess")
-var disLettersGuessed = document.getElementById("#wrong-letters")
-var disCorrect = document.getElementById("#reveal")
-//game
-currentWord = wordList[Math.floor(Math.random() * wordList.length)]
-console.log(currentWord)
+    //storage
+    var numWins = 0
+    var numGuesses = 10
+    var wrongLetters = []
+    var wordList = ["dragonborn", "dwarf", "elf", "gnome", "halfelf", "halforc", "halfling", "human", "tiefling", "barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "warlock", "wizard"]
+    var currentWord = ""
+    var correct = []
+    //display
+    var disWins = document.getElementById("dis-wins")
+    var disNumGuess = document.getElementById("num-guess")
+    var disLettersGuessed = document.getElementById("wrong-letters")
+    var disCorrect = document.getElementById("reveal")
+    //game
+    var wordSelect = function(){
+        currentWord = wordList[Math.floor(Math.random() * wordList.length)]
+        correct = []
+        for (var i = 0; i < currentWord.length; i++){
+            correct.push("_")
+        }
+    }
 
+    var letterCheck = function(guess){
+        var counter = 0
+        for (var i = 0; i < currentWord.length; i++){
+            if (guess === currentWord[i]){
+                correct[i] = guess
+                counter++
+            }
+        }
+        if (counter < 1 && wrongLetters.indexOf(guess) === -1){
+            wrongLetters.push(guess)
+        }
+    }
+    
+    document.addEventListener("keyup", function(event){
+        letterCheck(event.key.toLowerCase())
+    })
 
 
 
