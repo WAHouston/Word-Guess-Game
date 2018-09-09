835ec3ef9db554ee7d1661a61d9fec83b9f8c723
Will H <wandrewhouston@gmail.com>
2018-09-09 15:51:31 -0400

    frame and variable

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//storage
+var numWins = 0
+var numGuesses = 10
+var lettersGuessed = []
+var wordList = ["dragonborn", "dwarf", "elf", "gnome", "halfelf", "halforc", "halfling", "human", "tiefling", "barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "warlock", "wizard"]
+var currentWord = ""
+var correct = []
+//display
+var disWins = document.getElementById("#dis-wins")
+var disNumGuess = document.getElementById("#num-guess")
+var disLettersGuessed = document.getElementById("#wrong-letters")
+var disCorrect = document.getElementById("#reveal")
